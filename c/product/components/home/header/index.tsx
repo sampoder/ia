bcffbbbd9c89c610264144be8efe696ac1c26301
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-04-13 21:04:44 +0800

    Format

--- c/product/components/home/header/index.tsx
+++ c/product/components/home/header/index.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className={styles.header}>
+    <div
+      className={styles.header}
+    >
       <div>
         <h1 className={styles.title}>
           Welcome To A Global
@@ -11,7 +13,7 @@ export default function Header() {
         </h1>
         <p>
           <Link href="#tournaments">
-            <button style={{marginRight: '10px'}}>Join A Tournament</button>
+            <button style={{ marginRight: "10px" }}>Join A Tournament</button>
           </Link>
           <Link href="/event/new">
             <button>Organise A Tournament</button>
