480d0c73925662c6861a7b87f8385c5bae904475
Sam Poder <39828164+sampoder@users.noreply.github.com>
2022-04-15 15:51:08 +0800

    Scaffold Tab Management System

--- c/product/components/admin/wrapper/index.tsx
+++ c/product/components/admin/wrapper/index.tsx
@@ -6,35 +6,105 @@ export default function AdminWrapper(props: {
   children: ReactChild | ReactChildren;
   slug: string;
   name: string;
+  tab?: boolean | undefined;
 }) {
+  let tab = props.tab != undefined ? props.tab : false;
   return (
     <div className={styles.wrapper}>
       <div className={styles.sideBar}>
         <div className={styles.navigation}>
           <b>{props.name}</b>
-          <h3 className={styles.adminTitle}>Admin Dashboard</h3>
-          <div className={styles.links}>
-            <div>
-              <Link href={`/event/${props.slug}/admin/configure`}>
-                Configure Event
-              </Link>
+          <h3 className={styles.adminTitle}>
+            {!tab ? "Admin Dashboard" : "Tab Management"}
+          </h3>
+          {!tab && (
+            <div className={styles.links}>
+              <div>
+                <Link href={`/event/${props.slug}/admin/configure`}>
+                  Configure Event
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/attendees`}>
+                  View Attendees
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/email`}>
+                  Email Attendees
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/team`}>
+                  Manage Organising Team
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/tab`}>
+                  Tab Management
+                </Link>
+              </div>
             </div>
-            <div>
-              <Link href={`/event/${props.slug}/admin/attendees`}>
-                View Attendees
-              </Link>
+          )}
+          {tab && (
+            <div className={styles.links}>
+              <div>
+                <Link href={`/event/${props.slug}/admin/configure`}>
+                ↪ Admin Dashboard
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/attendees`}>
+                  Draw Rules
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/attendees`}>
+                  Debate Rules
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/email`}>
+                  Scoring Rules
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/email`}>
+                  Standings
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/email`}>
+                  Motions
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/team`}>
+                  Tab Release
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/team`}>
+                  Data Entry
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/team`}>
+                  Notifications
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/team`}>
+                  Adjudicators
+                </Link>
+              </div>
+              <div>
+                <Link href={`/event/${props.slug}/admin/team`}>
+                  Rooms
+                </Link>
+              </div>
             </div>
-            <div>
-              <Link href={`/event/${props.slug}/admin/email`}>
-                Email Attendees
-              </Link>
-            </div>
-            <div>
-              <Link href={`/event/${props.slug}/admin/team`}>
-                Manage Organising Team
-              </Link>
-            </div>
-          </div>
+          )}
         </div>
       </div>
       <div className={styles.content}>{props.children}</div>
---
+++ c/product/pages/event/[slug]/admin/tab/index.tsx
@@ -0,0 +1,83 @@
+import { GetServerSideProps } from "next";
+import {
+  User as UserType,
+  Tournament as TournamentType,
+  Team as TeamType,
+  UserTeamRelationship,
+} from "@prisma/client";
+import { getAdminProps } from "../../../../../lib/methods/load-admin-props";
+import Nav from "../../../../../components/nav";
+import Wrapper from "../../../../../components/admin/wrapper";
+import styles from "./styles.module.css";
+import Link from "next/link";
+
+function Team(props: {
+  team: TeamType & { members: (UserTeamRelationship & { user: UserType })[] };
+  tournament: TournamentType;
+}) {
+  return (
+    <details className={styles.team}>
+      <summary>{props.team.name}</summary>
+      <ul>
+        {props.team.members.map((member) => (
+          <li>
+            {member.user.firstName} {member.user.lastName}: {member.user.email}
+          </li>
+        ))}
+      </ul>
+      <Link href={`/api/event/${props.tournament.slug}/${props.team.id}/deregister`}>
+        Remove From Tournament
+      </Link>
+    </details>
+  );
+}
+
+export default function AdminTeam(props: {
+  user: UserType | undefined;
+  tournament: TournamentType;
+  organisers: UserType[];
+  teams: (TeamType & {
+    members: (UserTeamRelationship & { user: UserType })[];
+  })[];
+}) {
+  return (
+    <>
+      <Nav user={props.user} />
+      <Wrapper tab={true} slug={props.tournament?.slug} name={props.tournament?.name}>
+        <>
+          {" "}
+          <div>
+            <h1 className="adminHeader">Attendees</h1>
+            {props.teams.map((team) => (
+              <Team team={team} tournament={props.tournament} />
+            ))}
+          </div>
+          <div className={styles.form}>
+            <form
+              action={`/api/event/${props.tournament?.slug}/register?organiser=true`}
+              method="POST"
+              className="flexFormWrapper"
+            >
+              <b>Add A Team</b>
+              <input name="name" placeholder="Team Name" />
+              <small className={styles.emailsLabel}>
+                Team Members' Emails:{" "}
+              </small>
+              {[...Array(3)].map((_, index) => (
+                <input
+                  placeholder={`Team Member ${index + 1}'s Email`}
+                  name={`email${index}`}
+                  key={`email${index}`}
+                />
+              ))}
+
+              <button>Register</button>
+            </form>
+          </div>
+        </>
+      </Wrapper>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = getAdminProps;
